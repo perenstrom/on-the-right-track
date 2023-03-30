f50598a2a070b1ff6fa468b9e9f01f9fcd205aa7
Per Enström <enstrom.per@gmail.com>
2023-03-30 16:40:00 +0200

    Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,118 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+import Head from 'next/head';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700&family=Mulish:ital,wght@0,300;0,500;1,300;1,500&display=swap');
+
+  // RESET
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+  }
+  
+  html, body {
+    height: 100%;
+  }
+  
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+  
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+  
+  input, button, textarea, select {
+    font: inherit;
+  }
+  
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+  
+  #root, #__next {
+    isolation: isolate;
+    height: 100%;
+  }
+  // END RESET
+
+  // GLOBAL STYLES
+  body {
+    font-family: 'Mulish', sans-serif;
+    font-weight: 300;
+    line-height: 1.75;
+    color: hsl(0 0% 15%);
+    background-color: hsl(60 30% 97%);
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 3rem 0 1.38rem;
+    font-family: 'Cinzel', serif;
+    font-weight: 700;
+    line-height: 1.3;
+  }
+  
+  h1 {
+  margin-top: 0;
+  font-size: 4.209rem;
+}
+
+  h2 { font-size: 3.157rem; }
+
+  h3 { font-size: 2.369rem; }
+
+  h4 { font-size: 1.777rem; }
+
+  h5 { font-size: 1.333rem; }
+
+  small { font-size: 0.75rem; }
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  @media (max-width: 768px) {
+    h1 {
+      margin-top: 0;
+      font-size: 1.802rem;
+    }
+
+    h2 {font-size: 1.602rem;}
+
+    h3 {font-size: 1.424rem;}
+
+    h4 {font-size: 1.266rem;}
+
+    h5 {font-size: 1.125rem;}
+
+    small {font-size: 0.889rem;}
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  return <Component {...pageProps} key={router.asPath} />;
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <title>Den Riktiga Finalen</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} key={router.asPath} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,91 @@
 import { NextPage } from 'next';
-import Head from 'next/head';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   max-width: 700px;
   margin: 0 auto;
+  margin-top: 6rem;
+  padding: 0 1rem;
+
+
+  @media (max-width: 768px) {
+    margin-top: 3rem;
+  }
 `;
 
 const IndexPage: NextPage<{}> = ({}) => {
   return (
     <Wrapper>
-      <Head>
-        <title>NextJS Typescript Starter</title>
-      </Head>
-      <h1>Opinionated NextJS Typescript starter</h1>
+      <h1>Den Riktiga Finalen</h1>
       <p>
-        This is my preferred starter template for building NextJS apps in
-        Typescript.
+        Competition is a fundamental aspect of human society that drives
+        innovation, progress, and growth. It refers to a situation where two or
+        more individuals or groups are vying for the same resources or goals,
+        creating a dynamic where each participant must strive to outperform the
+        others. In many cases, competition can be healthy and beneficial, as it
+        motivates people to work harder, be more creative, and think outside the
+        box. However, it can also be destructive, leading to unethical behavior,
+        sabotage, and cutthroat tactics that harm both the individuals involved
+        and the larger community.
+      </p>
+      <h2>Den Riktiga Finalen</h2>
+      <p>
+        Competition is a fundamental aspect of human society that drives
+        innovation, progress, and growth. It refers to a situation where two or
+        more individuals or groups are vying for the same resources or goals,
+        creating a dynamic where each participant must strive to outperform the
+        others. In many cases, competition can be healthy and beneficial, as it
+        motivates people to work harder, be more creative, and think outside the
+        box. However, it can also be destructive, leading to unethical behavior,
+        sabotage, and cutthroat tactics that harm both the individuals involved
+        and the larger community.
+      </p>
+      <h3>Den Riktiga Finalen</h3>
+      <p>
+        Competition is a fundamental aspect of human society that drives
+        innovation, progress, and growth. It refers to a situation where two or
+        more individuals or groups are vying for the same resources or goals,
+        creating a dynamic where each participant must strive to outperform the
+        others. In many cases, competition can be healthy and beneficial, as it
+        motivates people to work harder, be more creative, and think outside the
+        box. However, it can also be destructive, leading to unethical behavior,
+        sabotage, and cutthroat tactics that harm both the individuals involved
+        and the larger community.
+      </p>
+      <h4>Den Riktiga Finalen</h4>
+      <p>
+        Competition is a fundamental aspect of human society that drives
+        innovation, progress, and growth. It refers to a situation where two or
+        more individuals or groups are vying for the same resources or goals,
+        creating a dynamic where each participant must strive to outperform the
+        others. In many cases, competition can be healthy and beneficial, as it
+        motivates people to work harder, be more creative, and think outside the
+        box. However, it can also be destructive, leading to unethical behavior,
+        sabotage, and cutthroat tactics that harm both the individuals involved
+        and the larger community.
+      </p>
+      <h5>Den Riktiga Finalen</h5>
+      <p>
+        Competition is a fundamental aspect of human society that drives
+        innovation, progress, and growth. It refers to a situation where two or
+        more individuals or groups are vying for the same resources or goals,
+        creating a dynamic where each participant must strive to outperform the
+        others. In many cases, competition can be healthy and beneficial, as it
+        motivates people to work harder, be more creative, and think outside the
+        box. However, it can also be destructive, leading to unethical behavior,
+        sabotage, and cutthroat tactics that harm both the individuals involved
+        and the larger community.
+      </p>
+      <p>
+        In the business world, competition is a ubiquitous force that drives
+        companies to improve their products, services, and strategies.
+        Businesses compete for customers, market share, and profits, creating a
+        highly dynamic and ever-evolving landscape that rewards innovation and
+        adaptability. While competition can be intense and stressful, it can
+        also be highly rewarding for those who excel, providing opportunities
+        for growth, recognition, and success. Ultimately, competition plays a
+        crucial role in shaping our society and driving progress in all areas of
+        life.
       </p>
     </Wrapper>
   );
